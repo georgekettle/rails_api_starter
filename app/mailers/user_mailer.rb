36e7a63f86ffbd7e0bc047c9c7f97ea811c01095
george kettle <georgekettle.gk@gmail.com>
2025-06-10 19:31:07 +1000

    CORS and user mailer updated

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
 
   def password_reset(user, token)
     @user = user
-    @reset_url = "#{ENV.fetch('FRONTEND_URL', 'http://localhost:3000')}/reset-password?token=#{token}"
+    @reset_url = "#{ENV.fetch('FRONTEND_URL', 'http://localhost:3000')}/auth/reset-password?token=#{token}"
     
     mail(
       to: @user.email,
@@ -11,3 +11,19 @@ class UserMailer < ApplicationMailer
     )
   end
 end 
+
+
+render ReactiveContainer.new do
+  render Trigger.new(:cart_items, event: :click, reaction: add_to_cart({name: "Shirt", price: 100})) do |cart_items|
+    render Button.new { "Add to Cart" }
+  end
+
+  div do
+    render Value.new(cart_items: [{name: "Dress", count: 2, price: 100}]) do |cart_items|
+      p { "#{cart_items.count} item(s) in cart" }
+      cart_items.each do |item|
+        render CartItem.new(item)
+      end
+    end
+  end
+end
